74e0d630d44e55524e25bb0661ed33465c94960d
Camilo Garzón <Camilogzn19@gmail.com>
2022-04-29 17:02:03 -0500

    adding styles and creating form validation

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import Formulario from './components/Formulario';
 
 function App() {
   return (
-    <div className="App">
-    </div>
+    <>
+    <Formulario />
+    </>
   );
 }
 
